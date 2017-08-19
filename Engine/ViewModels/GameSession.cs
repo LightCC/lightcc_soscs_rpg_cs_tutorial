02fd33826628b266f1240adf58f1b2ab63a01bf0
Mike Leiter <light_cc@yahoo.com>
2017-08-19 02:08:37 -0400

    03.4: Creating the GameSession (ViewModel) Class
    * Added Engine.ViewModels.GameSession.cs class
    * Added GameSession core class with CurrentPlayer object

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,16 @@
+﻿using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Mike";
+            CurrentPlayer.Gold = 100000;
+        }
+    }
+}
