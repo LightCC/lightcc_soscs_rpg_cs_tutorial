ab0825fb36c0040646266c9d2b5a738946a15468
Mike Leiter <light_cc@yahoo.com>
2017-08-19 02:52:36 -0400

    Cleanup of auto gen function

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -37,9 +37,5 @@ namespace WPFUI
             _gameSession.CurrentPlayer.XP += 10;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
