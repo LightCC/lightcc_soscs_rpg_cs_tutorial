d305d5c044b11a1df5af4b7bb83c058d37d714ec
Mike Leiter <light_cc@yahoo.com>
2017-08-20 01:37:49 -0400

    04.3-b Moving in the Game World * Update MainWindow to have movement buttons * Add MoveTo<North/West/East/South> methods in GameSession * Update GameSession to use INotifyPropertyChanged interface * Add event PropertyChanged and method OnPropertyChanged to GameSession * Notify MainWindow when location changes to update Location information * Add HasLocationTo<North/West/East/South> properties to GameSession to determine if there is a location to move to in each direction * Add BooleanToVisibility converter resource to MainWindow.xaml * Update MainWindow movement buttons to be visible based on "HasLocationTo<Direction>" * Attach new Move<Direction> functions to each of the movement keys

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,13 +1,28 @@
 ﻿using Engine.Models;
 using Engine.Factories;
+using System.ComponentModel;
 
 namespace Engine.ViewModels
 {
-    public class GameSession
+    public class GameSession : INotifyPropertyChanged
     {
+        private Location _location;
+
         public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
-        public Location CurrentLocation { get; set; }
+        public Location CurrentLocation
+        {
+            get { return _location; }
+            set
+            {
+                _location = value;
+                OnPropertyChanged("CurrentLocation");
+                OnPropertyChanged("HasLocationToNorth");
+                OnPropertyChanged("HasLocationToWest");
+                OnPropertyChanged("HasLocationToEast");
+                OnPropertyChanged("HasLocationToSouth");
+            }
+        }
 
         public GameSession()
         {
@@ -26,5 +41,73 @@ namespace Engine.ViewModels
             CurrentLocation = CurrentWorld.LocationAt(0, -1);
             CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool HasLocationToNorth
+        {
+            get
+            {
+                return (CurrentWorld.LocationAt(CurrentLocation.XCoordinate,
+                    CurrentLocation.YCoordinate + 1) != null);
+            }
+        }
+
+        public bool HasLocationToWest
+        {
+            get
+            {
+                return (CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1,
+                    CurrentLocation.YCoordinate) != null);
+            }
+        }
+
+        public bool HasLocationToEast
+        {
+            get
+            {
+                return (CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1,
+                    CurrentLocation.YCoordinate) != null);
+            }
+        }
+
+        public bool HasLocationToSouth
+        {
+            get
+            {
+                return (CurrentWorld.LocationAt(CurrentLocation.XCoordinate,
+                    CurrentLocation.YCoordinate - 1) != null);
+            }
+        }
+
+        public void MoveNorth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate,
+                CurrentLocation.YCoordinate + 1);
+        }
+
+        public void MoveWest()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1,
+                CurrentLocation.YCoordinate);
+        }
+
+        public void MoveEast()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1,
+                CurrentLocation.YCoordinate);
+        }
+
+        public void MoveSouth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate,
+                CurrentLocation.YCoordinate - 1);
+        }
+
     }
 }
