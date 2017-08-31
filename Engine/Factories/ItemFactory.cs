c151a19d9dd2000ec2e257a4cf23f1917e4ad63a
Mike Leiter <light_cc@yahoo.com>
2017-08-30 23:29:21 -0400

    7.1 Refactor - New weapons and GameItems with IDs as enums

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -11,17 +11,30 @@ namespace Engine.Factories
     public static class ItemFactory
     {
         private static List<GameItem> _standardGameItems;
+        private enum EGameItems
+        {
+            POINTY_STICK = 1001,
+            RUSTY_SWORD = 1002,
+            SNAKE_FANG = 9001,
+            SNAKESKIN = 9002,
+            RAT_TAIL = 9003,
+            RAT_FUR = 9004,
+            SPIDER_FANG = 9005,
+            SPIDER_SILK = 9006
+        }
 
         static ItemFactory()
         {
-            _standardGameItems = new List<GameItem>
-            {
-                new Weapon(1001, "Pointy Stick", 1, 1, 1),
-                new Weapon(1002, "Rusty Sword", 5, 1, 3),
-                new GameItem(9001, "Snake fang", 1),
-                new GameItem(9002, "Snakeskin", 2)
-            };
-            // Alternate: _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 1));
+            _standardGameItems = new List<GameItem>();
+
+            _standardGameItems.Add(new Weapon((int)EGameItems.POINTY_STICK, "Pointy Stick", 1, 1, 2));
+            _standardGameItems.Add(new Weapon((int)EGameItems.RUSTY_SWORD, "Rusty Sword", 5, 1, 3));
+            _standardGameItems.Add(new GameItem((int)EGameItems.SNAKE_FANG, "Snake fang", 1));
+            _standardGameItems.Add(new GameItem((int)EGameItems.SNAKESKIN, "Snakeskin", 2));
+            _standardGameItems.Add(new GameItem((int)EGameItems.RAT_TAIL, "Rat tail", 1));
+            _standardGameItems.Add(new GameItem((int)EGameItems.RAT_FUR, "Rat fur", 2));
+            _standardGameItems.Add(new GameItem((int)EGameItems.SPIDER_FANG, "Spider fang", 1));
+            _standardGameItems.Add(new GameItem((int)EGameItems.SPIDER_SILK, "Spider silk", 2));
         }
 
         public static GameItem CreateGameItem(int itemTypeID)
